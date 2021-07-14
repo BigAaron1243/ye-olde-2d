5e2f0da645530d744d8d247e4f3d0a11b0a4cc9e
Deejideej <athrowawayemailtwo@gmail.com>
2021-07-15 09:09:36 +1000

    ye olde commit

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int rounder(float d)
+{
+  return floor(d + 0.5);
+}
+
+class Circle {
+    public:
+        //radius, x-coordinate, y-coordinate, x-velocity, y-velocity, mass, coefficient of friction (mu)
+        float r, x, y, vx, vy, m, u;
+        void set_values(float,float,float,int);
+        std::vector<std::vector<float>> edge;
+
+};
+
+void Circle::set_values (float setr, float setx, float sety, int setresolution) {
+    r = setr; x = setx; y = sety;
+    for (int i = 0; i < setresolution; i++) {
+        edge.push_back({sin(i*((2*M_PI)/setresolution)) * setr,cos(i*((2*M_PI)/setresolution)) * setr});
+    }
+}
+
+void print_window (int plane[100][100]) {
+    std::string canvas;
+    for(size_t y = 0; y < 100; y++) {
+        char line[200];
+        for(size_t x = 0; x < 100; x++) {
+            if(plane[x][y] == 1) {
+                line[x * 2] = '0';
+                line[x * 2 - 1] = '0';
+            } else {
+                line[x * 2] = ' ';
+                line[x * 2 - 1] = ' ';
+            }
+        }
+        canvas.append(line);
+        canvas.append("\n");
+    }
+    std::cout << canvas << std::endl;
+}
+
+int main()
+{
+    std::vector<Circle> circle_list;
+    Circle ball;
+    ball.set_values(10,50,50,300);
+    circle_list.push_back(ball);
+    int canvas[100][100] = {0};
+    while (true) {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                canvas[i][j] = 0;
+            }
+        }
+        for (size_t i = 0; i < circle_list.size(); i++) {
+            circle_list[i].x++;
+            circle_list[i].y+=.5;
+            for (size_t j = 0; j < circle_list[i].edge.size(); j++) {
+                canvas[rounder(circle_list[i].edge[j][0] + circle_list[i].x)][rounder(circle_list[i].edge[j][1] + circle_list[i].y)] = 1;
+            }
+        }
+
+        system("cls");
+        print_window(canvas);
+    }
+    return 0;
+}
